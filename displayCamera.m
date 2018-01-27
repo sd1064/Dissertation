5efecc3b8ad08350b6326e0f2d308aa9e52e1cf1
sd1064 <sd1064@york.ac.uk>
2018-01-27 17:39:28 +0000

    Implemented perspective projection

--- displayCamera.m
+++ displayCamera.m
@@ -1,28 +1,18 @@
-%TO DO SHOW SPHERE AND CAMERA 
 X = [outputOne(1,1);outputTwo(1,1)];
 Y = [outputOne(1,2);outputTwo(1,2)];
 Z = [outputOne(1,3);outputTwo(1,3)];
 
-figure;
+figure;hold on;axis equal;grid on;
 plot3(X,Y,Z,'g*');
-hold on
-orientation = eye(3);
-location = [0 ; 0 ; 0 ];
-cam = plotCamera('Location',location,'Orientation',orientation,'Size',20);
-set(gca,'CameraUpVector',[0 0 -1]);
-camorbit(gca,-110,60,'data',[0 0 1]);
 
-axis equal
-grid on
-cameratoolbar('SetMode','orbit');
+view([-180 90])
+
+cam = plotCamera('Location',[0 ; 0 ; 0 ],'Orientation',eye(3),'Size',20);
+
 
 xlabel('X (mm)');
 ylabel('Y (mm)');
 zlabel('Z (mm)');
-
-xlim([-outputOne(1,1)*0.25 outputOne(1,1)*1.5 ]);
-ylim([-outputOne(1,2)*1.5 outputOne(1,2)*1.5 ]);
-zlim([0 outputOne(1,3)*1.5 ]);
 
 txt1 = 'Sphere One';
 txt2 = 'Sphere Two';
--- main.m
+++ main.m
@@ -1,3 +1,8 @@
+clc;
+clear;
+
+%%
+
 % Setup
 % Calibrate Camera
 % Read in image 
@@ -6,26 +11,44 @@
 % Make call to calculate extrinsic
 % Displays the camera and sphere
 
+% originalImage = imread('test.jpg');
+% cameraParams = calibrateCamera(9);
+% [undistortedImage, newOrigin] = undistortImage(originalImage, cameraParams, 'OutputView', 'full');
+% [centres,radii] = circleRecognition(undistortedImage);
+% 
+% sphereOne = [centres(1,1) centres(1,2) radii(1,1)];
+% sphereTwo = [centres(2,1) centres(2,2) radii(2,1)];
+% 
+% outputOne = extrinsicSphereCalibration(cameraParams.IntrinsicMatrix,sphereOne,8);
+% outputTwo = extrinsicSphereCalibration(cameraParams.IntrinsicMatrix,sphereTwo,8);
+% 
+% displayCamera;
 
-clc;
-clear;
+%%
 
-originalImage = imread('test.jpg');
-cameraParams = calibrateCamera(9);
-[undistortedImage, newOrigin] = undistortImage(originalImage, cameraParams, 'OutputView', 'full');
-[centres,radii] = circleRecognition(undistortedImage);
+% load scanned face
+% load face list from morhable model
+% rotate face so faces towards "camera" and set slightly back
+% add in a camera for looks
+% use a dummy value for focal length for now
+% could do with scaling everything down
 
-sphereOne = [centres(1,1) centres(1,2) radii(1,1)];
-sphereTwo = [centres(2,1) centres(2,2) radii(2,1)];
+load('00001_20061015_00418_neutral_face05.mat');
+load('01_MorphableModel.mat','tl');
 
-outputOne = extrinsicSphereCalibration(cameraParams.IntrinsicMatrix,sphereOne,8);
-outputTwo = extrinsicSphereCalibration(cameraParams.IntrinsicMatrix,sphereTwo,8);
+verts=(roty(180)*shape).';
+verts(:,3)= verts(:,3)+5e5; 
 
-displayCamera;
+figure;hold on;
+pcshow([verts(:,1),verts(:,2),verts(:,3)]);
+xlabel('X');ylabel('Y');zlabel('Z');axis equal;grid on;
 
-%input mesh
+F = 1e5;
+cam = plotCamera('Location',[0 ; 0 ; 0 ],'Orientation',eye(3),'Size',1e4);
+projectedImage = perspectiveProjection(verts,F);
 
-
+zPos = repelem(F,size(projectedImage,1)).';
+pcshow([projectedImage(:,1),projectedImage(:,2),zPos])
 
 
 
