122bc4da60ff101c2068200b2e70c3f377667248
Simon Devine <sd1064@its.york.ac.uk>
2018-02-20 09:53:33 +0000

    Added temporary correct panoramic image

--- scripts/sphereUndistort.m
+++ scripts/sphereUndistort.m
@@ -1,25 +1,35 @@
 clc;
 clear all;
 
-originalImage = imread('lightProbe.bmp');
-[centres,radii] = circleRecognition(originalImage,450,500);
-sphereOne = [centres(1,1) centres(1,2) radii(1,1)];
-spherePanoramic = [round(sphereOne(1)) round(sphereOne(2)) round(sphereOne(3))];
+originalImage = imread('ParkAngularMap-800.jpg');
+originalImage = im2double(originalImage);
+
+
+%[centres,radii] = circleRecognition(originalImage,450,500);
+%sphereOne = [centres(1,1) centres(1,2) radii(1,1)];
+%spherePanoramic = [round(sphereOne(1)) round(sphereOne(2)) round(sphereOne(3))];
+
+spherePanoramic = [size(originalImage,1)/2 size(originalImage,1)/2 size(originalImage,1)/2];
 pixelList = zeros((spherePanoramic(3)*2) * (spherePanoramic(3)*2),5);
 
-i = 1; 
-for x = (spherePanoramic(1)-spherePanoramic(3)):(spherePanoramic(1)+spherePanoramic(3))
-    for y = (spherePanoramic(2)-spherePanoramic(3)):(spherePanoramic(2)+spherePanoramic(3))
-        if sqrt((x - spherePanoramic(1))^2+(y-spherePanoramic(2))^2) < spherePanoramic(3)
-            
-          u = x - (spherePanoramic(1)-spherePanoramic(3));
-          v = y - (spherePanoramic(2)+spherePanoramic(3));
+i = 1;
+for row = (spherePanoramic(2)-spherePanoramic(3)):(spherePanoramic(2)+spherePanoramic(3))
+    for col = (spherePanoramic(1)-spherePanoramic(3)):(spherePanoramic(1)+spherePanoramic(3))
+        if sqrt((row - spherePanoramic(2))^2+(col-spherePanoramic(1))^2) < spherePanoramic(3)
           
+          % Calculate u,v
+          uImageCoord = col - (spherePanoramic(2)-spherePanoramic(3));
+          vImageCoord = row - (spherePanoramic(1)-spherePanoramic(3));
+          u = (1/spherePanoramic(3))*uImageCoord -1;
+          v = (1/spherePanoramic(3))*vImageCoord -1;
+          
+          % Calculate theta,phi
           pixelList(i,1) = atan2(v,u);
           pixelList(i,2) = pi*sqrt((u)^2 + (v)^2);
-          pixelList(i,3) = originalImage(y,x,1);
-          pixelList(i,4) = originalImage(y,x,2);
-          pixelList(i,5) = originalImage(y,x,3);
+          
+          pixelList(i,3) = originalImage(col,row,1);
+          pixelList(i,4) = originalImage(col,row,2);
+          pixelList(i,5) = originalImage(col,row,3);
           
         end
         i = i+1;
@@ -28,11 +38,23 @@ end
 
 pixelList = pixelList(any(pixelList,2),:);
 
-x = 0:1000;
-y = 0:2000;
-[X,Y] = meshgrid(x,y);
+redInterp   = scatteredInterpolant(pixelList(:,1),pixelList(:,2),pixelList(:,3));
+greenInterp = scatteredInterpolant(pixelList(:,1),pixelList(:,2),pixelList(:,4));
+blueInterp  = scatteredInterpolant(pixelList(:,1),pixelList(:,2),pixelList(:,5));
 
-% F = zeros(size(X,1),size(X,2));
-% surf(X,Y,F);axis equal;
+imageHeight = 400;
+% cols = linspace(0,2*pi,2*imageHeight);
+% rows = linspace(0,pi,imageHeight);
 
-% use scattered interpolator
+cols = linspace(0,pi,imageHeight);
+rows = linspace(0,pi,2*imageHeight);
+
+[C,R] = meshgrid(cols,rows);
+
+r   = redInterp(C,R);
+g   = greenInterp(C,R);
+b   = blueInterp(C,R);
+
+rgbImage = cat(3, r, g, b);
+rgbImage = imrotate(rgbImage,90);
+figure;imshow(rgbImage)
